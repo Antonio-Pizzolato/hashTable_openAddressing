e26cce19355c91f744b9e8d5f9b40322c48124ca
Antonio-Pizzolato <antonio.pizzolato@stud.unifi.it>
2023-04-20 19:20:12 +0200

    randomizzazione

--- main.py
+++ main.py
@@ -2,15 +2,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import array as arr
+import random
 import time
+import numpy as np
 
-elements = [int(item) for item in
-            input("Enter the elements which are going to be inserted in the hash table: ").split()]
+# elements = [int(item) for item in
+#             input("Enter the elements which are going to be inserted in the hash table: ").split()]
+# print(elements)
+#
+# elements_number = len(elements)
+# print('Number of elements: ', elements_number)
+
+
+elements_number = int(input("Enter the length of the array that will be randomly created: "))
+elements = np.random.randint(0, 9999, elements_number)
 print(elements)
-
-elements_number = len(elements)
-print('Number of elements: ', elements_number)
-
 
 # linear hash function (non solo con coefficiente 1)
 def linearHash(x, i):
@@ -58,12 +64,15 @@ class HashTable(object):
         i = 0
         while True:
             k = int(HashFunction(value, i))
+            # if k >= elements_number:
+            #     print("Error")
+            #     return
             if HashTable.table[k] == HashTable.empty_cell or HashTable.deleted_cell:
                 HashTable.table[k] = value
                 return k
             else:
                 i = i + 1
-            if i == HashTable.default_size:
+            if i >= HashTable.default_size:
                 print("Error: Hash Table Overflow")
                 return
 
@@ -101,14 +110,16 @@ end = time.perf_counter()
 print('\n', 'Hash Table: ', T.table)
 print('Time in seconds to insert elements into the Hash Table: '"%10.10f" % (end - start), '\n')
 
+searched = int(input("Insert the value you are looking for in the hash table: "))
 start = time.perf_counter()
-a = HashTable.search(T, 2)
+a = HashTable.search(T, searched)
 end = time.perf_counter()
 print('Index of the searched element: ', a)
 print('Time in seconds to look for the element into the Hash Table: '"%10.10f" % (end - start), '\n')
 
+deleted = int(input("Insert the value you are looking for in the hash table: "))
 start = time.perf_counter()
-HashTable.delete(T, 2)
+HashTable.delete(T, deleted)
 end = time.perf_counter()
 print('Hash Table with deleted element: ', T.table)
 print('Time in seconds to delete the element into the Hash Table: '"%10.10f" % (end - start))
